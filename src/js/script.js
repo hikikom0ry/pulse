b87bf3c3202a23f7dfb7804e36fe7e1f33663d3f
Zaur Tsallagov <example@gmail.com>
2024-04-08 00:58:34 +0300

    Lesson 3-17-3 not complete

--- src/js/script.js
+++ src/js/script.js
@@ -57,6 +57,12 @@ $(document).ready(function() {
     });
 
     $('.button_mini').on('click', function() {
-        
+        $('.overlay, #order').fadeIn();
+    })
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        })
     })
 });
